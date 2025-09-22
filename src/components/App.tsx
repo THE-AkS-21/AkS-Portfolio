7b3078bfb5486448d82095236931c6050ace5c77
Ankit <ankit.s2117@gmail.com>
2025-09-22 20:53:31 +0530

    UPDATE

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
-import { PortfolioLayout } from './PortfolioLayout'; // We'll create this next
+import { useState } from 'react';
+import { PortfolioLayout } from './PortfolioLayout';
 import { ProjectPage } from '../pages/ProjectPage';
+import { Navbar } from './Navbar';
+import { Footer } from './Footer';
+import { MobileMenu } from './MobileMenu';
 
 function App() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <Routes>
-            <Route path="/" element={<PortfolioLayout />} />
-            <Route path="/project/:projectId" element={<ProjectPage />} />
-        </Routes>
+        <div className="min-h-screen flex flex-col app-bg text-gray-200">
+            <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <main className="flex-grow">
+                <Routes>
+                    <Route path="/" element={<PortfolioLayout />} />
+                    <Route path="/project/:projectId" element={<ProjectPage />} />
+                </Routes>
+            </main>
+            <Footer />
+        </div>
     );
 }
 
